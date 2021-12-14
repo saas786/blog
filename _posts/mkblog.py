c516120b7ea2d347906ab627571b7acf2a835977
loflv <419426468@qq.com>
2021-12-14 21:15:49 +0800

    笔记更新

---
+++ _posts/mkblog.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import time  # 引入time模块
+import sys
+
+
+ 
+ticks = time.time()
+filename =  time.strftime("%Y-%m-%d", time.localtime()) + "-"+sys.argv[1]+".md"
+
+print(filename)
+with open('./'+filename, 'w') as f:
+    f.write("---\n")
+    f.write("layout: post\n")
+    f.write("title: ")
+    f.write(filename+"\n")
+    f.write("categories: \n")
+    f.write("description: \n")
+    f.write("keywords: \n")
+    f.write("---\n")
+    f.write("\n")
+    f.write("简介\n")
+    f.write("\n")
+    f.write("\n")
+    f.write("内容\n")
+    f.write("\n")
